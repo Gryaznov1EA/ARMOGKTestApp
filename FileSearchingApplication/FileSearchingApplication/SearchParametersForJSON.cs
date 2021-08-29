0f26928ba1db4551db26f24ed97605fc27507884
Gryaznov1EA <86610259+Gryaznov1EA@users.noreply.github.com>
2021-08-29 15:34:22 +0300

    Add files via upload

---
+++ FileSearchingApplication/FileSearchingApplication/SearchParametersForJSON.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSearchingApplication
+{
+    class SearchParametersForJSON
+    {
+        public string RootPath { get; set; }
+        public string FilenameMask { get; set; }
+    }
+}
